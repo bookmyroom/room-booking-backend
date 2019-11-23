362b609dd73f090914f62f6dd96a788ac20565b3
Switchu <remplak@gmail.com>
2019-11-23 10:45:25 +0100

    spring boot initialized project

---
+++ src/main/java/pl/booking/bookmyroom/BookmyroomApplication.java
@@ -0,0 +1,13 @@
+package pl.booking.bookmyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/booking/bookmyroom/BookmyroomApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.booking.bookmyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
