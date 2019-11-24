b8ce62cb42593d5d5834dace6047bc4798d5fa7e
Switchu <remplak@gmail.com>
2019-11-24 12:08:16 +0100

    #15 mysql db integration implemented

--- src/main/java/pl/booking/bookmyroom/reservation/repository/ReservationRepository.java
+++ src/main/java/pl/booking/bookmyroom/reservation/repository/ReservationRepository.java
@@ -1,9 +1,15 @@
 package pl.booking.bookmyroom.reservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.booking.bookmyroom.reservation.model.Reservation;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+    @Query(value = "SELECT r FROM Reservation r WHERE r.roomTypeId=roomTypeId")
+    List<Reservation> findAllByRoomId(Integer roomTypeId);
 }
